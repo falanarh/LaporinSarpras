d184ffb5250b4effedd36c71a0610348c9046cdd
222112038 <falanarofako29@gmail.com>
2023-12-17 21:05:25 +0700

    berhasil baca data dengan GET

--- app/src/main/java/com/polstat/laporinsarpras/model/Pengaduan.kt
+++ app/src/main/java/com/polstat/laporinsarpras/model/Pengaduan.kt
@@ -15,23 +15,40 @@ data class Pengaduan(
     val emailTeknisi: String?,
     val barangId: String,
     val ruangId: String,
-    @Serializable(with = LocalDateSerializer::class)
-    val tanggal: LocalDate,
+    val tanggal: List<Int>,
     val prioritas: String,
     val status: String,
-    val keterangan: String,
+    val keterangan: String?,
     val deskripsi: String
-)
-
-@Serializer(forClass = LocalDate::class)
-object LocalDateSerializer : KSerializer<LocalDate> {
-    private val formatter = DateTimeFormatter.ISO_LOCAL_DATE
-
-    override fun serialize(encoder: Encoder, value: LocalDate) {
-        encoder.encodeString(value.format(formatter))
-    }
-
-    override fun deserialize(decoder: Decoder): LocalDate {
-        return LocalDate.parse(decoder.decodeString(), formatter)
-    }
+) {
+    val date: LocalDate
+        get() = LocalDate.of(tanggal[0], tanggal[1], tanggal[2])
 }
+
+//@Serializable
+//data class Pengaduan(
+//    val pengaduanId: Long,
+//    val emailPelapor: String,
+//    val emailTeknisi: String?,
+//    val barangId: String,
+//    val ruangId: String,
+//    @Serializable(with = LocalDateSerializer::class)
+//    val tanggal: LocalDate,
+//    val prioritas: String,
+//    val status: String,
+//    val keterangan: String,
+//    val deskripsi: String
+//)
+//
+//@Serializer(forClass = LocalDate::class)
+//object LocalDateSerializer : KSerializer<LocalDate> {
+//    private val formatter = DateTimeFormatter.ISO_LOCAL_DATE
+//
+//    override fun serialize(encoder: Encoder, value: LocalDate) {
+//        encoder.encodeString(value.format(formatter))
+//    }
+//
+//    override fun deserialize(decoder: Decoder): LocalDate {
+//        return LocalDate.parse(decoder.decodeString(), formatter)
+//    }
+//}
